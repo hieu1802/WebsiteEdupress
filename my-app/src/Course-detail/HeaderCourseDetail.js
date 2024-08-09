4daf39384ec860bb158d16cb47c4aa9ef3461b6c
Lam Quyen <chauquyen1233@gmail.com>
2024-08-09 17:53:41 +0700

    commit

---
+++ my-app/src/Course-detail/HeaderCourseDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const headerCourseDetail = () => {
+    return (
+        <>
+            <div className="header-course-detail">
+                <p className='course-name'>{nameCourse} by {teacher}</p>
+                <h1 className='general-course'>The Ultimate Guide to the best WordPress LMS Plugin</h1>
+                <div className='about-course'></div>
+            </div>
+        </>
+    )
+
+}
