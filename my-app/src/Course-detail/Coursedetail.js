cf77c39922654ef040a708a5e9c7683f601b918f
Lam Quyen <chauquyen1233@gmail.com>
2024-08-10 12:16:06 +0700

    quyen

---
+++ my-app/src/Course-detail/Coursedetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderCourseDetail from './HeaderCourseDetail';
+import PriceCourseCard from './PriceCourseCard';
+import './CourseDetail.css'
+const CourseDetail = () => {
+    return (
+        <>
+
+            <HeaderCourseDetail />
+            <PriceCourseCard />
+
+        </>
+    )
+}
+export default CourseDetail;
--- my-app/src/Course-detail/HeaderCourseDetail.js
+++ my-app/src/Course-detail/HeaderCourseDetail.js
@@ -1,13 +1,31 @@
 import React from 'react';
-const headerCourseDetail = () => {
+import Levels from './image/alllevels.png';
+import Student from './image/student.png';
+import Time from './image/time.png';
+import Lesson from './image/lesson.png';
+import Quizz from './image/quizz.png';
+import './CourseDetail.css';
+
+
+const HeaderCourseDetail = () => {
     return (
         <>
-            <div className="header-course-detail">
-                <p className='course-name'>{nameCourse} by {teacher}</p>
-                <h1 className='general-course'>The Ultimate Guide to the best WordPress LMS Plugin</h1>
-                <div className='about-course'></div>
+            <div className="course-detail">
+                <div className="course-header">
+                    <span className="badge">Photography</span>
+                    <span className="author">by Determined-Poitras</span>
+                </div>
+                <h1 className="course-title">The Ultimate Guide To The Best WordPress LMS Plugin</h1>
+                <div className="course-info">
+                    <span className="info-item"><img src={Time} /> 2Weeks</span>
+                    <span className="info-item"><img src={Student} /> 156 Students</span>
+                    <span className="info-item"><img src={Levels} />All levels</span>
+                    <span className="info-item"><img src={Lesson} /> 20 Lessons</span>
+                    <span className="info-item"><img src={Quizz} /> 3 Quizzes</span>
+                </div>
             </div>
         </>
     )
 
 }
+export default HeaderCourseDetail;
