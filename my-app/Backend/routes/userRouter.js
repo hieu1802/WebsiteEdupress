60672def1eed0da755fde142c12e308e68fe6e27
Lam Quyen <chauquyen1233@gmail.com>
2024-10-28 15:03:16 +0700

    2810

--- my-app/Backend/routes/userRouter.js
+++ my-app/Backend/routes/userRouter.js
@@ -6,34 +6,26 @@ import {
   deleteUserById,
   getAllUsers,
   getUserById,
-<<<<<<< HEAD
-  updateUserById, deleteComment
-} from '../controllers/userController.js';
-import express from 'express';
-import multer from 'multer';
-import { storage } from '../config/cloundinary.js';
-=======
-  updateUserById,
-  deleteComment,
+  updateUserById, deleteComment,
   requestPasswordReset,
   resetPassword,
-} from "../controllers/userController.js";
+} from '../controllers/userController.js';
+
+import multer from 'multer';
+import { storage } from '../config/cloundinary.js';
+//   updateUserById,
+//   deleteComment,
+//   requestPasswordReset,
+//   resetPassword,
+// } from "../controllers/userController.js";
 import express from "express";
->>>>>>> 8713d9150b0c93d6f6fb1454f205b63fc8973c34
 const router = express.Router();
 const upload = multer({ storage });
 
-<<<<<<< HEAD
 router.post('/create-comment/:courseId', upload.single('image'), createComment)
 router.put('/update-comment/:commentId', updateComment);
 router.put('/hide-comment/:commentId', hideComment)
 router.delete('/delete-comment/:commentId', deleteComment)
-=======
-router.post("/create-comment/:courseId", createComment);
-router.put("/update-comment/:commentId", updateComment);
-router.put("/hide-comment/:commentId", hideComment);
-router.delete("/delete-comment/:commentId", deleteComment);
->>>>>>> 8713d9150b0c93d6f6fb1454f205b63fc8973c34
 // import {
 //   changePassword,
 //   createComment,
